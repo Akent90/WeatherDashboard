a5a294daabfbe1de0288699a079fd8d3045be48b
akent <alexanderkent099@gmail.com>
2024-01-05 12:06:26 -0500

    Add initial DOM elements

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function(){
+    const searchForm = document.getElementById('searchForm');
+    const searchInput = document.getElementById('searchInput');
+    const currentWeatherDiv = document.getElementById('currentWeather');
+    const forecastDiv = document.getElementById('forecast');
+    const historyList = document.getElementById('historyList');
+    const apiKey = 'e5d02244f2fdf8cc53a789407a0475c6';
+
+    
+})
